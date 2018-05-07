d20e3baece125cfc34dfb1fd08a6a1be994700b9
Christian Eichelmann <ceichelmann@gmx.de>
2018-05-07 13:22:51 +0200

    remove worker from cluster on delete

--- provisioner.tf
+++ provisioner.tf
@@ -34,6 +34,25 @@ resource "null_resource" "worker_init" {
   }
 }
 
+resource "null_resource" "worker_remove" {
+  when  = "destroy"
+  count = "${var.swarm_worker_count}"
+
+  triggers {
+    cluster_instance_ids = "${digitalocean_droplet.docker_swarm_worker.*.id[count.index]}"
+  }
+
+  connection {
+    host = "${digitalocean_droplet.docker_swarm_worker.*.ipv4_address[count.index]}"
+    user = "${var.do_user}"
+    agent = true
+  }
+
+  provisioner "local-exec" {
+    command = "cioctl remove"
+  }
+}
+
 resource "null_resource" "master_deploy" {
   triggers {
     cluster_instance_ids = "${null_resource.worker_init.*.id[0]}"
