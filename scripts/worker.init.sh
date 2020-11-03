cd94f4f3180a398028d955db003b734454ee727e
root <root@DESKTOP-8UMVKPU.localdomain>
2020-11-03 15:52:05 -0800

    Fix script to scale up cluster (TODO: fix to scale down)

--- scripts/worker.init.sh
+++ scripts/worker.init.sh
@@ -9,9 +9,10 @@ SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 # TODO: wait for ssh to become available
 sleep 30
 
-jointoken="$(ssh ${SSH_OPTIONS} ${SSH_USER}@${SSH_ADDRESS_SDS} 'cioctl join-token | grep "cioctl add"')"
+jointoken="$(ssh ${SSH_OPTIONS} ${SSH_USER}@${SSH_ADDRESS_SDS} 'cioctl join-token | grep "cioctl node add"')"
 if [ $? -ne 0 ]; then
     echo "no cluster yet adding node to initial config..."
+    echo "${jointoken}"
     scp ${SSH_OPTIONS} sds_key ${SSH_USER}@${SSH_ADDRESS}:sds_key.pem
     command="$(cat storidge.token | grep 'cioctl join') --ip ${PRIVATE_ADDRESS}"
     ssh ${SSH_OPTIONS} ${SSH_USER}@${SSH_ADDRESS} "${command}"
@@ -22,3 +23,5 @@ else
     ssh ${SSH_OPTIONS} ${SSH_USER}@${SSH_ADDRESS} "${command}"
 fi
 
+# TODO: remove net-error status node if there is no corresponding droplet to private IP after scaling down
+# leavetoken="$(ssh ${SSH_OPTIONS} ${SSH_USER}@${SSH_ADDRESS_SDS} 'cio node ls | grep "net-error"')"
